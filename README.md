8fc26c7ef2ab0e2cc5cb59b43761ba923d8be83b
trisha <trisha@teredesai.com>
2025-11-11 12:45:35 -0800

    add dataset & update readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# geospatial-analysis-project
+## Geospatial Analysis: Analysis of Frequent Locations and Movement Patterns
+
+to run,
+source path/to/venv/bin/activate
+cd repo/geospatial-analysis-project
+python3 code.py
---
+++ code.py
@@ -0,0 +1,15 @@
+import pandas as pd
+# import numpy as np
+# import statsmodels.api as sm
+# import statsmodels.formula.api as smf
+# from sklearn.model_selection import train_test_split
+# from sklearn.neighbors import KNeighborsClassifier
+# from sklearn.naive_bayes import GaussianNB
+# from sklearn.metrics import confusion_matrix, accuracy_score
+# from sklearn.preprocessing import StandardScaler
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+# from ISLP import load_data
+
+locations64 = pd.read_csv('locations/locations_64.csv')
+print(locations64.head())
